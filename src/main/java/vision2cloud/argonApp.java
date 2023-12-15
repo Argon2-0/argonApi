be3daabbec42173c199cd2076c0644719cfbd182
RichardUG <rsug.rsug@gmail.com>
2023-12-15 11:22:46 -0500

    servlet

--- src/main/java/vision2cloud/argonApp.java
+++ src/main/java/vision2cloud/argonApp.java
@@ -3,13 +3,15 @@ package vision2cloud;
 import org.postgresql.util.PSQLException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @ComponentScan(basePackages={"vision2cloud.argon"})
-public class argonApp {
+public class argonApp extends SpringBootServletInitializer {
     public static void main(String[] args) throws PSQLException {
         try {
             SpringApplication.run(argonApp.class, args);
@@ -17,4 +19,9 @@ public class argonApp {
             System.out.println(e);
         }
     }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(argonApp.class);
+    }
+
 }
