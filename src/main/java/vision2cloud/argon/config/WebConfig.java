d1836302eb2ba8aa91e9f33567a78aa49ed05395
RichardUG <rsug.rsug@gmail.com>
2024-03-10 22:43:22 -0500

    time

--- src/main/java/vision2cloud/argon/config/WebConfig.java
+++ src/main/java/vision2cloud/argon/config/WebConfig.java
@@ -3,10 +3,13 @@ package vision2cloud.argon.config;/*
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.TimeZone;
 
 @Configuration
 @EnableWebMvc
@@ -14,5 +17,10 @@ public class WebConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("GET", "POST","PUT", "DELETE");;
+    }
+
+    @Bean
+    public void setTimeZone(){
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Bogota"));
     }
 }
